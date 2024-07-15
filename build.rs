4cab506013c7215cf76f1d0a7196a590a9af6eea
Leonardo Held <leonardo.held@toradex.com>
2024-07-15 13:24:42 -0300

    Disable the build.rs thingy
    Signed-off-by: Leonardo Held <leonardo.held@toradex.com>

--- build.rs
+++
@@ -1,9 +0,0 @@
-use vergen::ShaKind;
-use vergen::{vergen, Config};
-
-fn main() {
-    // Generate the default 'cargo:' instruction output
-    let mut config = Config::default();
-    *config.git_mut().sha_kind_mut() = ShaKind::Both;
-    vergen(config).unwrap();
-}
